ef3e3d8f841050e8610319a26a17392ac136726b
rok <roky.zz@gmail.com>
2018-10-16 21:15:58 +0200

    completed all functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,37 +14,89 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
+
     suite('GET /api/stock-prices => stockData object', function() {
-      
+
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
-          done();
+			const data = JSON.parse(res.text);
+
+			assert.isNull(err)
+			assert.equal(data.stockData.stock, "GOOG");
+
+			done();
         });
       });
-      
+
       test('1 stock with like', function(done) {
-        
+		chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: true})
+        .end(function(err, res){
+			const data = JSON.parse(res.text);
+
+			assert.isNull(err);
+			assert.equal(data.stockData.stock, "GOOG");
+			assert.equal(data.stockData.likes, 1);
+
+			done();
+        });
       });
-      
+
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+		chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: true})
+        .end(function(err, res){
+			const data = JSON.parse(res.text);
+
+			assert.isNull(err)
+			assert.equal(data.stockData.stock, "GOOG");
+			assert.equal(data.stockData.likes, 1);
+
+			done();
+        });
       });
-      
+
       test('2 stocks', function(done) {
-        
+		chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['goog', 'aapl']})
+        .end(function(err, res){
+			const data = JSON.parse(res.text);
+
+			assert.isNull(err)
+			assert.isArray(data.stockData)
+			assert.isOk(data.stockData.map(item => item.stock).includes("GOOG"));
+			assert.isOk(data.stockData.map(item => item.stock).includes("AAPL"));
+			assert.equal(data.stockData[0].rel_likes, 0);
+			assert.equal(data.stockData[1].rel_likes, 0);
+
+			done();
+        });
       });
-      
+
       test('2 stocks with like', function(done) {
-        
+		chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['goog', 'aapl'], like: true})
+        .end(function(err, res){
+			const data = JSON.parse(res.text);
+
+			assert.isNull(err)
+			assert.isArray(data.stockData)
+			assert.isOk(data.stockData.map(item => item.stock).includes("GOOG"));
+			assert.isOk(data.stockData.map(item => item.stock).includes("AAPL"));
+			assert.equal(data.stockData[0].rel_likes, 0);
+			assert.equal(data.stockData[1].rel_likes, 0);
+
+			done();
+        });
       });
-      
+
     });
 
 });
